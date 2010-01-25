34dfd68a5ec8bc9dfd40c5d25c44ae1e6a6f0685
kdidenko <kdidenko@e5e4f178-09b3-11df-b317-0f3410dc57f6>
2010-01-25 14:59:22 +0000

    Removed old references

--- ito-mvc/html/global-includes.php
+++ ito-mvc/html/global-includes.php
@@ -1,9 +1,5 @@
 <?php
 	include_once 'global-settings.php';
-	include_once 'aliases.php';
-
-	# append third-party libraries include path
-	set_include_path(get_include_path() . PATH_SEPARATOR . 'lib');
 
 	include_once 'com/ito-global/services/xml/XsltHandler.php';
 
@@ -14,13 +10,5 @@
 	include_once 'com/ito-global/services/messages/MessageService.php';
 	include_once 'com/ito-global/services/messages/LocalizationFactory.php';
     include_once 'com/ito-global/services/http/HttpHelper.php';
-
-	include_once 'com/ito/expopharm/services/SessionService.php';
-	include_once 'com/ito/expopharm/services/StorageService.php';
-	include_once 'com/ito/expopharm/services/ScanningService.php';
-	include_once 'com/ito/expopharm/services/OrderService.php';
-	include_once 'com/ito/expopharm/services/AdsService.php';
-
-	include_once 'com/google/doclist/ocr/GoogleOcrClient.php';
 
 ?>

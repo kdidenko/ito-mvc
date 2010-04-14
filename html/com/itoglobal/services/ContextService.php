e19dd7566f30e4ec9f81a1a84990e26889351476
astabryn <astabryn@e5e4f178-09b3-11df-b317-0f3410dc57f6>
2010-04-14 11:31:20 +0000

    fixed according for code review for revision r51

--- html/com/itoglobal/services/ContextService.php
+++ html/com/itoglobal/services/ContextService.php
@@ -4,82 +4,105 @@ require_once 'com/itoglobal/mvc/defaults/BaseActionControllerImpl.php';
 
 class ContextService extends BaseActionControllerImpl {
 	
+	/**
+	 * @var string - alias for new web site
+	 */
 	const ALIAS = 'alias';
-	const DOMEN = 'dom';
+	/**
+	 * @var string - domain for new web site
+	 */
+	const DOMAIN = 'dom';
+	/**
+	 * @var string - domain which will be deleted
+	 */
 	const DELETE = 'delete';
+	/**
+	 * @var string - context directory name 
+	 */
 	const CONTEXT = 'context';
+	/**
+	 * @var string - images directory name 
+	 */
 	const IMAGES = 'images';
+	/**
+	 * @var string - templates directory name 
+	 */
 	const TEMPLATES = 'templates';
+	/**
+	 * @var string - style directory name
+	 */
 	const STYLES = 'styles';
+	/**
+	 * @var string - inc directory name
+	 */
 	const INC = 'inc';
-	const PATH_SEPARATOR = '/'; 
+	const PATH_SEPARATOR = '/';
 	
 	public static $res;
 	public static $view;
 	
-	public function CreateContext($actionParams, $requestParams){
+	public function CreateContext($actionParams, $requestParams) {
 		// calling parent to get the model
 		$mvc = $this->handleActionRequest ( $actionParams, $requestParams );
 		
-		$domen = $_POST[self::DOMEN];
-		$alias = $_POST[self::ALIAS];
-		//self::$res = StorageService::initSessionStorage($domen);
+		$domen = $requestParams [self::DOMAIN];
+		$alias = $requestParams [self::ALIAS];
 		
 		/*$path = StorageService::getStoragePath($domen);
 		$list = StorageService::readDir($path);
         foreach ($list as $next){
        		self::$view .= $next . "<br/>"; 	
         }*/
-        
-        
-        //creating struct
-        self::CreateDirectory(self::CONTEXT . self::PATH_SEPARATOR);
-        self::CreateDirectory(self::IMAGES . self::PATH_SEPARATOR . $alias);
-        self::CreateDirectory(self::STYLES . self::PATH_SEPARATOR . $alias);
-        self::CreateDirectory(self::TEMPLATES . self::PATH_SEPARATOR . $domen . self::PATH_SEPARATOR . self::INC . self::PATH_SEPARATOR);
 		
-        self::CreateFile(self::CONTEXT . self::PATH_SEPARATOR . $domen . '-mapping.xml');
-	    self::CreateFile(self::TEMPLATES . self::PATH_SEPARATOR . $domen . self::PATH_SEPARATOR . 'template.xml');
-		self::CreateFile(self::TEMPLATES . self::PATH_SEPARATOR . $domen . self::PATH_SEPARATOR . self::INC . self::PATH_SEPARATOR . 'index.html',  "Hello Word, I am " . $domen);
+		//creating struct
+		self::CreateDirectory ( self::CONTEXT . self::PATH_SEPARATOR );
+		self::CreateDirectory ( self::IMAGES . self::PATH_SEPARATOR . $alias );
+		self::CreateDirectory ( self::STYLES . self::PATH_SEPARATOR . $alias );
+		self::CreateDirectory ( self::TEMPLATES . self::PATH_SEPARATOR . $domen . self::PATH_SEPARATOR . self::INC . self::PATH_SEPARATOR );
+		
+		self::CreateFile ( self::CONTEXT . self::PATH_SEPARATOR . $domen . '-mapping.xml' );
+		self::CreateFile ( self::TEMPLATES . self::PATH_SEPARATOR . $domen . self::PATH_SEPARATOR . 'template.xml' );
+		self::CreateFile ( self::TEMPLATES . self::PATH_SEPARATOR . $domen . self::PATH_SEPARATOR . self::INC . self::PATH_SEPARATOR . 'index.html', "Hello Word, I am " . $domen );
 		
 		return $mvc;
-	} 
+	}
 	
-	public function DeleteContext($actionParams, $requestParams){
+	public function DeleteContext($actionParams, $requestParams) {
 		// calling parent to get the model
 		$mvc = $this->handleActionRequest ( $actionParams, $requestParams );
 		
-		$dir = $_POST[self::DELETE];
-		self::DeleteDirectory($dir);
+		$dir = $_POST [self::DELETE];
+		self::DeleteDirectory ( $dir );
 		
 		return $mvc;
-	} 
+	}
 	
-	private function CreateDirectory($path){
-		file_exists($path) ? '' : mkdir($path, 0755, true) ;
-	} 
+	private function CreateDirectory($path) {
+		file_exists ( $path ) ? '' : mkdir ( $path, 0755, true );
+	}
 	
 	private function CreateFile($path, $context = null) {
-		if( !file_exists($path)){
-	        $path = fopen($path, 'w') or die("can't open file");
-			isset($context) ? fwrite($path, $context) : fwrite($path, $context);
-	        fclose($path);
-        }
+		if (! file_exists ( $path )) {
+			$path = fopen ( $path, 'w' ) or die ( "can't open file" );
+			isset ( $context ) ? fwrite ( $path, $context ) : fwrite ( $path, $context );
+			fclose ( $path );
+		}
 	}
-		
-	private function DeleteDirectory($dir) {
-        if (!file_exists($dir)) return true;
-        if (!is_dir($dir)) return unlink($dir);
-        foreach (scandir($dir) as $item) {
-            if ($item == '.' || $item == '..') continue;
-            if (!self::deleteDirectory($dir . self::PATH_SEPARATOR . $item)) return false;
-        }
-        return rmdir($dir);
-    }
 	
-
+	private function DeleteDirectory($dir) {
+		if (! file_exists ( $dir ))
+			return true;
+		if (! is_dir ( $dir ))
+			return unlink ( $dir );
+		foreach ( scandir ( $dir ) as $item ) {
+			if ($item == '.' || $item == '..')
+				continue;
+			if (! self::deleteDirectory ( $dir . self::PATH_SEPARATOR . $item ))
+				return false;
+		}
+		return rmdir ( $dir );
+	}
 
 }
-
 
 ?>

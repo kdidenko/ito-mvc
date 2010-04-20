cccc27b88140321ed3f6e95b83058b5e17ad8bec
astabryn <astabryn@e5e4f178-09b3-11df-b317-0f3410dc57f6>
2010-04-20 14:06:58 +0000

    0000002: Implement account registration functionality

--- branches/ito-nlp/html/com/itoglobal/users/RegistrationController.php
+++ branches/ito-nlp/html/com/itoglobal/users/RegistrationController.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once 'com/itoglobal/db/sql/mysql/SQLClient.php';
 require_once 'com/itoglobal/mvc/defaults/BaseActionControllerImpl.php';
 
 class RegistrationController extends BaseActionControllerImpl {
@@ -25,14 +25,13 @@ class RegistrationController extends BaseActionControllerImpl {
 	public function registration($actionParams, $requestParams) {
 		// calling parent to get the model
 		$mvc = $this->handleActionRequest ( $actionParams, $requestParams );
-		
+		if ($_POST){
 		self::$error = '';
-		self::$error .= self::CheckUsername ( $_POST[self::USERNAME] );
-		self::$error .= self::CheckName ( $_POST[self::FIRSTNAME] );
-		self::$error .= self::CheckLastname ( $_POST[self::LASTNAME] );
-		self::$error .= self::CheckEmail ( $_POST[self::EMAIL] );
-		self::$error .= self::CheckPassword ( $_POST[self::PASSWORD] );
-		self::$error .= self::CheckConfirmPassword ( $_POST[self::PASSWORD], $_POST[self::CONFIRM] );
+		self::$error .= self::checkUsername ( $requestParams[self::USERNAME] );
+		self::$error .= self::checkName ( $requestParams[self::FIRSTNAME] );
+		self::$error .= self::checkEmail ( $requestParams[self::EMAIL] );
+		self::$error .= self::checkPassword ( $requestParams[self::PASSWORD] );
+		self::$error .= self::checkConfirmPassword ( $requestParams[self::PASSWORD], $requestParams[self::CONFIRM] );
 		//self::$error .= self::GenerateBirthday($birth_day, $birth_month, $birth_year);
 		//TODO: create birthday field!        
 		
@@ -45,35 +44,36 @@ class RegistrationController extends BaseActionControllerImpl {
 		}
 		
 		// Insert new users to DB
-		$fields = self::FIRSTNAME . ', ' . self::LASTNAME . ', ' . self::EMAIL . ', ' . self::USERNAME . ', ' . self::PASSWORD . ', ' . self::CRDATE;
-		$values = "'" . $_POST [self::FIRSTNAME] . "','" . $_POST [self::LASTNAME] . "','" . $_POST [self::EMAIL] . "','" . $_POST [self::USERNAME] . "','" . $_POST [self::PASSWORD] . "','" . gmdate ( "Y-m-d H:i:s" ) . "'";
+		$fields = self::USERNAME . ', ' . self::FIRSTNAME . ', ' . self::LASTNAME . ', ' . self::EMAIL . ', ' . self::PASSWORD . ', ' . self::CRDATE;
+		$values = "'" . $requestParams [self::USERNAME] . "','" . $requestParams [self::FIRSTNAME] . "','" . $requestParams [self::LASTNAME] . "','" . $requestParams [self::EMAIL] . "','" . $requestParams [self::PASSWORD] . "','" . gmdate ( "Y-m-d H:i:s" ) . "'";
 		$into = self::USERS;
 		$link = SQLClient::connect ( 'ito_global', 'localhost', 'root', '' );
 		$result = SQLClient::execInsert ( $fields, $values, $into, $link );
-		
+		}
 		return $mvc;
+		
 	}
 	
 	
-	private function CheckUsername($username) {
+	private function checkUsername($username) {
 		if (!$username) {
 			return '<p>Please enter your User Name</p>';
 		}
 	}
 	
-	private function CheckName($name) {
+	private function checkName($name) {
 		if (!$name) {
 			return '<p>Please enter your First Name</p>';
 		}
 	}
 	
-	private function CheckLastname($lastname) {
+	private function checkLastname($lastname) {
 		if (!$lastname) {
 			return '<p>Please enter your Last Name</p>';
 		}
 	}
 	
-	private function CheckEmail($email) {
+	private function checkEmail($email) {
 		if ($email) {
 			if (! preg_match ( '/^(([^<>()[\]\\.,;:\s@"\']+(\.[^<>()[\]\\.,;:\s@"\']+)*)|("[^"\']+"))@((\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\])|(([a-zA-Z\d\-]+\.)+[a-zA-Z]{2,}))$/', $email )) {
 				return "<p>Wrong email. Please enter a correct email</p>";
@@ -83,7 +83,7 @@ class RegistrationController extends BaseActionControllerImpl {
 		}
 	}
 	
-	private function CheckPassword($password) {
+	private function checkPassword($password) {
 		if ($password) {
 			if (strlen ( $password ) < 6) {
 				return '<p>The password you provided must have at least 6 characters.</p>';
@@ -93,7 +93,7 @@ class RegistrationController extends BaseActionControllerImpl {
 		}
 	}
 	
-	private function CheckConfirmPassword($password, $confirm_password) {
+	private function checkConfirmPassword($password, $confirm_password) {
 		if ($confirm_password) {
 			if ($password != $confirm_password) {
 				return '<p>Confirm Password does not match the password.</p>';

49d43cfd2901d97399d08f643e776284557e69b4
astabryn <astabryn@e5e4f178-09b3-11df-b317-0f3410dc57f6>
2010-04-20 08:15:45 +0000

    Modife RequestDispatcher

---
+++ html/com/itoglobal/services/http/RequestDispatcher.php
@@ -0,0 +1,23 @@
+<?php
+
+
+//TODO: modify RequestDispatcher - modify RequestDispatcher class for different types requests handling
+//TODO: implement dispatchHttpRequest method
+
+class RequestDispatcher {
+
+	private static $instance = NULL; 
+	
+	public function dispatchHttpRequest($request){
+				
+	}
+	
+	private function __construct(){
+	}
+	
+	public function getInstance(){
+		return (self::$instance === NULL) ? self::$instance = new self() : self::$instance;
+	}
+}
+
+?>

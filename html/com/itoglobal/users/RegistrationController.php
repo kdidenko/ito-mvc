c0516aa0156921771217622b20fbe053c63abb40
astabryn <astabryn@e5e4f178-09b3-11df-b317-0f3410dc57f6>
2010-04-22 14:44:52 +0000

    displayed errors for user registration functionality

--- html/com/itoglobal/users/RegistrationController.php
+++ html/com/itoglobal/users/RegistrationController.php
@@ -3,7 +3,7 @@ require_once 'com/itoglobal/mvc/defaults/BaseActionControllerImpl.php';
 
 class RegistrationController extends BaseActionControllerImpl {
 	
-	public static $error = '';
+	public static $error = array();
 	
 	const ID = 'id';
 	
@@ -25,24 +25,25 @@ class RegistrationController extends BaseActionControllerImpl {
 	
 	const USERS = 'users';
 	
+	const ERROR = 'error';
+		
 	public function registration($actionParams, $requestParams) {
 		// calling parent to get the model
 		$mvc = $this->handleActionRequest ( $actionParams, $requestParams );
 		if ($_POST) {
-			self::$error = '';
-			self::$error .= self::checkUsername ( $requestParams [self::USERNAME] );
-			self::$error .= self::checkName ( $requestParams [self::FIRSTNAME] );
-			self::$error .= self::checkLastname ( $requestParams [self::LASTNAME] );
-			self::$error .= self::checkEmail ( $requestParams [self::EMAIL] );
-			self::$error .= self::checkPassword ( $requestParams [self::PASSWORD] );
-			self::$error .= self::checkConfirmPassword ( $requestParams [self::PASSWORD], $requestParams [self::CONFIRM] );
+			self::$error[0] = self::checkUsername ( $requestParams [self::USERNAME] );
+			self::$error[1] = self::checkName ( $requestParams [self::FIRSTNAME] );
+			self::$error[2] = self::checkLastname ( $requestParams [self::LASTNAME] );
+			self::$error[3] = self::checkEmail ( $requestParams [self::EMAIL] );
+			self::$error[4] = self::checkPassword ( $requestParams [self::PASSWORD] );
+			self::$error[5] = self::checkConfirmPassword ( $requestParams [self::PASSWORD], $requestParams [self::CONFIRM] );
 			//self::$error .= self::GenerateBirthday($birth_day, $birth_month, $birth_year);
 			//TODO: create birthday field!        
 			
 
 			//TODO: error reporting
 			//$error - mistakes
-			if (self::$error == NULL) {
+			if (self::$error[0] == NULL & self::$error[1] == NULL & self::$error[2] == NULL & self::$error[3] == NULL & self::$error[4] == NULL & self::$error[5] == NULL ) {
 				// Insert new users to DB
 				$fields = self::USERNAME . ', ' . self::FIRSTNAME . ', ' . self::LASTNAME . ', ' . self::EMAIL . ', ' . self::PASSWORD . ', ' . self::CRDATE . ', ' . self::VALIDATION;
 				$hash = md5 ( rand ( 1, 9999 ) );
@@ -55,10 +56,9 @@ class RegistrationController extends BaseActionControllerImpl {
 				$location = $this->onSuccess ( $actionParams );
 				$this->forwardActionRequest ( $location );
 			} else {
-				$location = $this->onFailure ( $actionParams );
-				$this->forwardActionRequest ( $location );
+				$mvc->addObject(self::ERROR, self::$error);
 			}
-		}
+		}	
 		return $mvc;
 	
 	}
@@ -80,49 +80,49 @@ class RegistrationController extends BaseActionControllerImpl {
 	
 	private function checkUsername($username) {
 		if (! $username) {
-			return '<p>Please enter your User Name</p>';
+			return 'Please enter your User Name';
 		}
 	}
 	
 	private function checkName($name) {
 		if (! $name) {
-			return '<p>Please enter your First Name</p>';
+			return 'Please enter your First Name';
 		}
 	}
 	
 	private function checkLastname($lastname) {
 		if (! $lastname) {
-			return '<p>Please enter your Last Name</p>';
+			return 'Please enter your Last Name';
 		}
 	}
 	
 	private function checkEmail($email) {
 		if ($email) {
 			if (! preg_match ( '/^(([^<>()[\]\\.,;:\s@"\']+(\.[^<>()[\]\\.,;:\s@"\']+)*)|("[^"\']+"))@((\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\])|(([a-zA-Z\d\-]+\.)+[a-zA-Z]{2,}))$/', $email )) {
-				return "<p>Wrong email. Please enter a correct email</p>";
+				return "Wrong email. Please enter a correct email";
 			}
 		} else {
-			return '<p>Please enter your email address.</p>';
+			return 'Please enter your email address.';
 		}
 	}
 	
 	private function checkPassword($password) {
 		if ($password) {
 			if (strlen ( $password ) < 6) {
-				return '<p>The password you provided must have at least 6 characters.</p>';
+				return 'The password you provided must have at least 6 characters.';
 			}
 		} else {
-			return '<p>Please enter password</p>';
+			return 'Please enter password';
 		}
 	}
 	
 	private function checkConfirmPassword($password, $confirm_password) {
 		if ($confirm_password) {
 			if ($password != $confirm_password) {
-				return '<p>Confirm Password does not match the password.</p>';
+				return 'Confirm Password does not match the password.';
 			}
 		} else {
-			return '<p>Please enter confirm password</p>';
+			return 'Please enter confirm password';
 		}
 	}
 

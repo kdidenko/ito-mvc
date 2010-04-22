406b2702c263aa2913166cc82e1f62a65572fab4
kdidenko <kdidenko@e5e4f178-09b3-11df-b317-0f3410dc57f6>
2010-04-22 14:47:41 +0000

    Template Engine updated in order to handle recursive action executions within any template

--- html/com/itoglobal/mvc/services/TemplateEngine.php
+++ html/com/itoglobal/mvc/services/TemplateEngine.php
@@ -1,34 +1,43 @@
 <?php
 class TemplateEngine {
-
-    const TYPE_HTML = 'HTML';
-
-    const TYPE_XSLT = 'XSLT';
-
-    const TYPE_FILE = 'FILE';
-
-    public static function run ($mvc) {
-    	$template = $mvc->getTemplate();
-        switch ((string) $template['type']) {
-            case self::TYPE_XSLT:
-                self::doXSLT($template);
-                break;
-            default:
-                define('TEMPLATE_PATH', (string) $template['path']);
-                include_once TEMPLATE_PATH;
-        }
-    }
-
-    private static function doXSLT ($template) {
-        if(isset($template->input) && self::TYPE_FILE == (string)$template->input['type']){
-            echo XsltHandler::getInstance()->transform((string)$template->input['value'],(string)$template['path']);
-        }
-    }
-    
-    public static function execute($action) {
-    	
-    	
-    }
+	
+	const TYPE_HTML = 'HTML';
+	
+	const TYPE_XSLT = 'XSLT';
+	
+	const TYPE_FILE = 'FILE';
+	
+	public static function run($mvc) {
+		$template = $mvc->getTemplate ();
+		switch (( string ) $template ['type']) {
+			case self::TYPE_XSLT :
+				self::doXSLT ( $template );
+				break;
+			default :
+				define ( 'TEMPLATE_PATH', ( string ) $template ['path'] );
+				include_once TEMPLATE_PATH;
+		}
+	}
+	
+	private static function doXSLT($template) {
+		if (isset ( $template->input ) && self::TYPE_FILE == ( string ) $template->input ['type']) {
+			echo XsltHandler::getInstance ()->transform ( ( string ) $template->input ['value'], ( string ) $template ['path'] );
+		}
+	}
+	
+	/** 
+	 * Wrapper method for including files from template 
+	 * @param string the $filename to include
+	 * @param ModelAndView the $mvc Object
+	 */
+	public static function inclusion($filename, $mvc = null) {
+		include_once $filename;
+	}
+	
+	public static function execute($action) {
+		$rd = RequestDispatcher::getInstance ();
+		TemplateEngine::run ( $rd->execute ( $action ) );
+	}
 
 }
 ?>

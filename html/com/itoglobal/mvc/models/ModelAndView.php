c86f07a3ed015ee66d55c888591867a21b31b2a6
kdidenko <kdidenko@e5e4f178-09b3-11df-b317-0f3410dc57f6>
2010-04-22 14:07:22 +0000

    Code clearence

--- html/com/itoglobal/mvc/models/ModelAndView.php
+++ html/com/itoglobal/mvc/models/ModelAndView.php
@@ -1,14 +1,14 @@
 <?php
 
 class ModelAndView {
-
+	
 	private $view = null;
 	
 	private $template = null;
-
+	
 	private $context = '';
-
-	private $model = array();
+	
+	private $model = array ();
 	/**
 	 * @return the $template
 	 */
@@ -28,57 +28,55 @@ class ModelAndView {
 		return $this->view;
 	}
 	/**
-     * @param $context the $context to set
-     */
-    public function setContext ($context) {
-        $this->context = $context;
-    }
-
+	 * @param $context the $context to set
+	 */
+	public function setContext($context) {
+		$this->context = $context;
+	}
+	
 	/**
-     * @return the $context
-     */
-    public function getContext () {
-        return $this->context != '' ? $this->context . '/' : '';
-    }
-
-
+	 * @return the $context
+	 */
+	public function getContext() {
+		return $this->context != '' ? $this->context . '/' : '';
+	}
+	
 	/**
 	 * @return the $model
 	 */
 	public function getModel() {
 		return $this->model;
 	}
-
+	
 	/**
 	 * @param $view the $view to set
 	 */
 	public function setView($view) {
 		$this->view = $view;
 	}
-
+	
 	/**
 	 * @param $model the $model to set
 	 */
 	public function setModel($model) {
 		$this->model = $model;
 	}
-
+	
 	/**
 	 * @param $name the $name of the $object to add to $model
 	 * @param $object the $object to add to $model
 	 */
 	public function addObject($name, $object) {
-		$this->model[$name] = $object;
+		$this->model [$name] = $object;
 	}
-
+	
 	/**
 	 * @param $name the $name of the $object to get from $model
 	 * @return the $object
 	 */
 	public function getObject($name) {
-		return key_exists($name, $this->model) ? $this->model[$name] : null;
+		return key_exists ( $name, $this->model ) ? $this->model [$name] : null;
 	}
-
 
 }
 

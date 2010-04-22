90ba34e99903e8a39261326acfa2e834ab6e2224
kdidenko <kdidenko@e5e4f178-09b3-11df-b317-0f3410dc57f6>
2010-04-22 15:13:19 +0000

    Templates path fixed

--- branches/ito-nlp/html/global-settings.php
+++ branches/ito-nlp/html/global-settings.php
@@ -2,6 +2,7 @@
 	define('DOCUMENT_ROOT', rtrim($_SERVER['DOCUMENT_ROOT'], '/\\') . '/');
 	define('STORAGE_PATH', DOCUMENT_ROOT . 'storage/');
 	define('SESSION_PATH', DOCUMENT_ROOT . 'storage/sessions/');
+	define('TEMPLATES_PATH', DOCUMENT_ROOT . 'templates/');	
 	define('XSLT_PATH', DOCUMENT_ROOT . 'templates/xslt/');
 
 	define('XML_CONTENT_DEFINITION', "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\r");

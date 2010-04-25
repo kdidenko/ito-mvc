9c565644c9b9033b9a2d08fe132ed0732532d545
astabryn <astabryn@e5e4f178-09b3-11df-b317-0f3410dc57f6>
2010-04-25 10:26:14 +0000

    fixed bug

--- html/com/itoglobal/lcms/controllers/SidebarController.php
+++ html/com/itoglobal/lcms/controllers/SidebarController.php
@@ -3,14 +3,14 @@
 require_once 'com/itoglobal/mvc/defaults/SecureActionControllerImpl.php';
 
 class SidebarController extends SecureActionControllerImpl {
-	
+	const USERS_ID = 'user_id';
 	/**
 	 * @param unknown_type unknown_type $actionParams
 	 * @param unknown_type unknown_type $requestParams
 	 * @return ModelAndView
 	 */
 	public function handleActionRequest($actionParams, $requestParams) {
-		$sid = SessionService::getSessionId ();
+		$sid = SessionService::getAttribute (self::USERS_ID);
 		if (! isset ( $sid )) {
 			TemplateEngine::inclusion ( $this->onSignedOff ( $actionParams ) );
 		} else {

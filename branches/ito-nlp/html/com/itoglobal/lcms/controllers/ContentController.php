31cfabb031f8d6abd251b641bb9a0a8027e0e23a
astabryn <astabryn@e5e4f178-09b3-11df-b317-0f3410dc57f6>
2010-04-25 14:43:40 +0000

    added ContentController and templates for navigation and content

---
+++ branches/ito-nlp/html/com/itoglobal/lcms/controllers/ContentController.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'com/itoglobal/mvc/defaults/SecureActionControllerImpl.php';
+
+class ContentController extends SecureActionControllerImpl {
+	/**
+	 * @param unknown_type unknown_type $actionParams
+	 * @param unknown_type unknown_type $requestParams
+	 * @return ModelAndView
+	 */
+	public function handleActionRequest($actionParams, $requestParams) {
+		$result = null;
+		$sid = SessionService::getAttribute ( SessionService::USERS_ID );
+		if (! isset ( $sid )) {
+			$result = RequestDispatcher::getInstance ()->dispatchActionRequest ( $this->onSignedOff ( $actionParams ) );
+		} else {
+			$result = parent::handleActionRequest ( $actionParams, $requestParams );
+		}
+		return $result;
+	}
+
+}
+
+?>

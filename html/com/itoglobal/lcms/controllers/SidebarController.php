ad2f0bb3b52d7a5c6841fa510a7881b058170372
kdidenko <kdidenko@e5e4f178-09b3-11df-b317-0f3410dc57f6>
2010-04-26 09:23:24 +0000

    Wildcart mapping for sidebar controller implemented

--- html/com/itoglobal/lcms/controllers/SidebarController.php
+++ html/com/itoglobal/lcms/controllers/SidebarController.php
@@ -3,15 +3,22 @@
 require_once 'com/itoglobal/mvc/defaults/SecureActionControllerImpl.php';
 
 class SidebarController extends SecureActionControllerImpl {
+	
 	/**
-	 * @param unknown_type unknown_type $actionParams
-	 * @param unknown_type unknown_type $requestParams
-	 * @return ModelAndView
+	 * Secure actions handling method implementation. Qualifies the
+	 * minimum required for sidebar Model View Controlling. 
+	 *
+	 * @see BaseActionController->handleActionRequest($actionParams, $requestParams)
 	 */
 	public function handleActionRequest($actionParams, $requestParams) {
 		return SessionService::isUserLogedin ( $actionParams, $requestParams ) ? 
 				SessionService::isUserLogedin ( $actionParams, $requestParams ) : 
 						SecureActionControllerImpl::handleActionRequest ( $actionParams, $requestParams );
+	}
+	
+	
+	public function handleHome($actionParams, $requestParams) {
+		return $this->handleActionRequest($actionParams, $requestParams);
 	}
 
 }

b02069794639f90a67924a69a64ec20aa820cfe6
kdidenko <kdidenko@e5e4f178-09b3-11df-b317-0f3410dc57f6>
2010-04-26 13:04:51 +0000

    Role handling core initial commit

--- branches/ito-nlp/html/com/itoglobal/lcms/controllers/ContentController.php
+++ branches/ito-nlp/html/com/itoglobal/lcms/controllers/ContentController.php
@@ -3,54 +3,44 @@
 require_once 'com/itoglobal/mvc/defaults/SecureActionControllerImpl.php';
 
 class ContentController extends SecureActionControllerImpl {
-	/**
-	 * @param unknown_type unknown_type $actionParams
-	 * @param unknown_type unknown_type $requestParams
-	 * @return ModelAndView
-	 */
-	public function handleActionRequest($actionParams, $requestParams) {
-		return SessionService::isLogedIn () ? 
-				RequestDispatcher::getInstance ()->dispatchActionRequest ( $this->onSignedOff ( $actionParams ) ) : 
-						SecureActionControllerImpl::handleActionRequest ( $actionParams, $requestParams );
-	}
 	
 	public function handleHome($actionParams, $requestParams) {
 		$mvc = $this->handleActionRequest ( $actionParams, $requestParams );
 		
 		$firstname = SessionService::getAttribute ( SessionService::FIRSTNAME );
-		isset( $firstname )? $mvc->addObject ( SessionService::FIRSTNAME, $firstname ): null;
+		isset ( $firstname ) ? $mvc->addObject ( SessionService::FIRSTNAME, $firstname ) : null;
 		$lastname = SessionService::getAttribute ( SessionService::LASTNAME );
-		isset( $lastname )? $mvc->addObject ( SessionService::LASTNAME, $lastname ): null;
+		isset ( $lastname ) ? $mvc->addObject ( SessionService::LASTNAME, $lastname ) : null;
 		
 		return $mvc;
 	}
 	
 	public function handleSchools($actionParams, $requestParams) {
-		return $this->handleActionRequest($actionParams, $requestParams);
+		return $this->handleActionRequest ( $actionParams, $requestParams );
 	}
 	public function handleTrainings($actionParams, $requestParams) {
-		return $this->handleActionRequest($actionParams, $requestParams);
+		return $this->handleActionRequest ( $actionParams, $requestParams );
 	}
 	public function handleCommunity($actionParams, $requestParams) {
-		return $this->handleActionRequest($actionParams, $requestParams);
+		return $this->handleActionRequest ( $actionParams, $requestParams );
 	}
 	public function handleDiscussions($actionParams, $requestParams) {
-		return $this->handleActionRequest($actionParams, $requestParams);
+		return $this->handleActionRequest ( $actionParams, $requestParams );
 	}
 	public function handleAbout($actionParams, $requestParams) {
-		return $this->handleActionRequest($actionParams, $requestParams);
+		return $this->handleActionRequest ( $actionParams, $requestParams );
 	}
 	public function handleMyResponses($actionParams, $requestParams) {
-		return $this->handleActionRequest($actionParams, $requestParams);
+		return $this->handleActionRequest ( $actionParams, $requestParams );
 	}
 	public function handleValuateResponses($actionParams, $requestParams) {
-		return $this->handleActionRequest($actionParams, $requestParams);
+		return $this->handleActionRequest ( $actionParams, $requestParams );
 	}
 	public function handleMyChallenges($actionParams, $requestParams) {
-		return $this->handleActionRequest($actionParams, $requestParams);
+		return $this->handleActionRequest ( $actionParams, $requestParams );
 	}
 	public function handleMessages($actionParams, $requestParams) {
-		return $this->handleActionRequest($actionParams, $requestParams);
+		return $this->handleActionRequest ( $actionParams, $requestParams );
 	}
 }
 
--- branches/ito-nlp/html/com/itoglobal/mvc/defaults/SecureActionControllerImpl.php
+++ branches/ito-nlp/html/com/itoglobal/mvc/defaults/SecureActionControllerImpl.php
@@ -23,6 +23,29 @@ class SecureActionControllerImpl extends BaseActionControllerImpl implements Sec
 	public function onLoggedIn($actionParams) {
 		return self::getLocationOnCondition ( $actionParams, self::MVC_ON_LOGGED_IN );
 	}
+	
+	/**
+	 * Secure actions handling method implementation. Qualifies the
+	 * minimum required for Model View Controlling and handles views based on User Role specified.
+	 * 
+	 * @param mixed $actionParams - action config parameters
+	 * @param mixed $requestParams - request parameters
+	 * @return ModelAndView
+	 */
+	public function handleActionRequest($actionParams, $requestParams) {
+		$result = parent::handleActionRequest ( $actionParams, $requestParams );
+		$template = $actionParams->template;
+		if (isset ( $template->role )) {
+			$role = SessionService::getRole ();
+			foreach ( $template->role as $key => $value ) {
+				if ($role == ( string ) $value ['type']) {
+					$result->setTemplate ( $value );
+					break;
+				}
+			}
+		}
+		return $result;
+	}
 
 }
 

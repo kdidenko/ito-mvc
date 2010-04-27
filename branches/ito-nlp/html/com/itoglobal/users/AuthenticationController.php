1a10dd1abcfc97971f84aec78ab33e9a83a1090c
astabryn <astabryn@e5e4f178-09b3-11df-b317-0f3410dc57f6>
2010-04-27 10:30:25 +0000

    - code clenup - remove unnecessary variables

--- branches/ito-nlp/html/com/itoglobal/users/AuthenticationController.php
+++ branches/ito-nlp/html/com/itoglobal/users/AuthenticationController.php
@@ -3,32 +3,7 @@
 require_once 'com/itoglobal/mvc/defaults/BaseActionControllerImpl.php';
 
 class AuthenticationController extends BaseActionControllerImpl {
-	
-	/**
-	 * @var  string defining the id field name
-	 */
-	const ID = 'id';
-	
-	/**
-	 * @var  string defining the username field name
-	 */
-	const USERNAME = 'username';
-	
-	/**
-	 * @var string defining the password field name
-	 */
-	const PASSWORD = 'password';
-	
-	/**
-	 * @var string defining the users table name
-	 */
-	const USERS = 'users';
-	
-	/**
-	 * @var string defining the enabled field name
-	 */
-	const ENABLED = 'enabled';
-	
+
 	/** User login handling controller method
 	 * @param the $actionParams
 	 * @param the $requestParams
@@ -37,22 +12,24 @@ class AuthenticationController extends BaseActionControllerImpl {
 	public function login($actionParams, $requestParams) {
 		$location = $this->onSuccess ( $actionParams );
 		# setting the query variables
-		$fields = self::ID . ', ' . self::USERNAME . ', ' . SessionService::FIRSTNAME . ', ' . SessionService::LASTNAME . ', ' . SessionService::EMAIL . ', ' . self::PASSWORD . ', ' . self::ENABLED . ', ' . SessionService::ROLE;
-		$from = self::USERS;
-		$where = self::USERNAME . " = '" . $requestParams [self::USERNAME] . "'";
+		$fields = UsersService::ID . ', ' . UsersService::USERNAME . ', ' . UsersService::FIRSTNAME . ', ' . UsersService::LASTNAME . ', ' . UsersService::EMAIL . ', ' . UsersService::PASSWORD . ', ' . UsersService::ENABLED . ', ' . UsersService::DELETED . ', ' . UsersService::ROLE;
+		$from = UsersService::USERS;
+		$where = UsersService::USERNAME . " = '" . $requestParams [UsersService::USERNAME] . "'";
 		# executing the query
 		$result = DBClientHandler::getInstance ()->execSelect ( $fields, $from, $where, '', '', '' );
 		# authenticating user login
-		if (count ( $result ) > 0 && $result [0] [self::ENABLED] == 1) {
-			if (isset ( $requestParams [self::PASSWORD] ) & $result [0] [self::PASSWORD] == md5 ( $requestParams [self::PASSWORD] )) {
-				$id = $result [0] [self::ID];
+		
+
+		if (count ( $result ) > 0 && $result [0] [UsersService::ENABLED] == 1 && $result [0] [UsersService::DELETED] == 0) {
+			if (isset ( $requestParams [UsersService::PASSWORD] ) & $result [0] [UsersService::PASSWORD] == md5 ( $requestParams [UsersService::PASSWORD] )) {
+				$id = $result [0] [UsersService::ID];
 				$session = SessionService::startSession ();
 				SessionService::setAttribute ( SessionService::USERS_ID, $id );
 				SessionService::setAttribute ( SessionService::USERNAME, $result [0] [SessionService::USERNAME] );
 				SessionService::setAttribute ( SessionService::FIRSTNAME, $result [0] [SessionService::FIRSTNAME] );
-				SessionService::setAttribute ( SessionService::LASTNAME, $result [0] [SessionService::LASTNAME]);
-				SessionService::setAttribute ( SessionService::EMAIL, $result [0] [SessionService::EMAIL]);
-				SessionService::setRole($result [0] [SessionService::ROLE]);
+				SessionService::setAttribute ( SessionService::LASTNAME, $result [0] [SessionService::LASTNAME] );
+				SessionService::setAttribute ( SessionService::EMAIL, $result [0] [SessionService::EMAIL] );
+				SessionService::setRole ( $result [0] [UsersService::ROLE] );
 			} else {
 				$location = $this->onFailure ( $actionParams );
 			}
@@ -63,7 +40,7 @@ class AuthenticationController extends BaseActionControllerImpl {
 	}
 	
 	public function logout($actionParams, $requestParams) {
-		SessionService::endSession();
+		SessionService::endSession ();
 		$location = $this->onSuccess ( $actionParams );
 		$this->forwardActionRequest ( $location );
 	}

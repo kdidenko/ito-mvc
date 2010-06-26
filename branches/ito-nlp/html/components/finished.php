209a788270151bab594fcdf3c41e459a2074d7ab
astabryn <astabryn@e5e4f178-09b3-11df-b317-0f3410dc57f6>
2010-06-26 12:59:54 +0000

    fixed video component

--- branches/ito-nlp/html/components/finished.php
+++ branches/ito-nlp/html/components/finished.php
@@ -3,7 +3,9 @@ include("header.php");
 ?>
 <p>Recording Result:
   <?=$_GET['result']?>
+  <?$vid=$_GET['stream'];?>
   
   
 </p>
-<p></b><a href="recorded_videos.php">Browse Video Recordings</a></p>
+<p><a href="recorded_videos.php">Browse Video Recordings</a></p>
+<p><a href="streamplay.php?vid=<?=($vid)?>">Play video</a></p>

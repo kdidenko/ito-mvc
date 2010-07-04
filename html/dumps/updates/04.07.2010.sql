146cac13313bedc651e97026470da90a6b72c6fe
astabryn <astabryn@e5e4f178-09b3-11df-b317-0f3410dc57f6>
2010-07-04 13:03:14 +0000

    import valuations data

--- html/dumps/updates/04.07.2010.sql
+++ html/dumps/updates/04.07.2010.sql
@@ -8,3 +8,10 @@ CREATE TABLE  `valuations` (
  `comment` VARCHAR( 1024 ) collate utf8_bin NOT NULL,
  `valuate` INT NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
+
+
+INSERT INTO `valuations` (`id`, `resp_index`, `user_id`, `comment`, `valuate`) VALUES 
+(2, 1, 37, 'I like this video', 2),
+(3, 2, 37, 'nice...', 1),
+(4, 1, 37, 'oh..  no...bad...very bad', -2),
+(6, 1, 37, 'I think, you chose a bad technique....', -1);

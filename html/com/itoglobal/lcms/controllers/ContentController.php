81ae9a4aa48f760500af02f6fbcf14cb4b67888e
astabryn <astabryn@e5e4f178-09b3-11df-b317-0f3410dc57f6>
2010-07-12 11:16:00 +0000

    fixed bug with displaying training exercises

--- html/com/itoglobal/lcms/controllers/ContentController.php
+++ html/com/itoglobal/lcms/controllers/ContentController.php
@@ -245,6 +245,10 @@ class ContentController extends SecureActionControllerImpl {
 	
 	public static function createTeaser ($list){
 		foreach($list as $key => $value){
+			$chr = strpos($value[SchoolService::DESCRIPTION], '</p>');
+			$value[SchoolService::DESCRIPTION] = $chr != NULL ? 
+				substr($value[SchoolService::DESCRIPTION],0,$chr) : 
+					$value[SchoolService::DESCRIPTION];
 			$value[SchoolService::DESCRIPTION] = substr($value[SchoolService::DESCRIPTION], 0, 255);
 			$list[$key][SchoolService::DESCRIPTION] = strrev(strstr(strrev($value[SchoolService::DESCRIPTION]), ' '));
 		}

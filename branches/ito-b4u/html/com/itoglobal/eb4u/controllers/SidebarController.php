03ee0d18724efff65ffc4d1b8c7ab0af6abaac65
astabryn <astabryn@e5e4f178-09b3-11df-b317-0f3410dc57f6>
2010-11-26 22:57:51 +0000

    display count of new mails in sidebar menu

--- branches/ito-b4u/html/com/itoglobal/eb4u/controllers/SidebarController.php
+++ branches/ito-b4u/html/com/itoglobal/eb4u/controllers/SidebarController.php
@@ -3,7 +3,21 @@
 require_once 'com/itoglobal/mvc/defaults/SecureActionControllerImpl.php';
 
 class SidebarController extends SecureActionControllerImpl {
-	
+	/**
+	 * @param unknown_type $actionParams
+	 * @param unknown_type $requestParams
+	 * @return $modelAndView
+	 */
+	public function handleActionRequest($actionParams, $requestParams) {
+		$mvc = parent::handleActionRequest($actionParams, $requestParams);
+		
+		$id = SessionService::getAttribute ( SessionService::USERS_ID );
+		$new_mails = MailService::countNew($id);
+		$mvc->addObject ( MailService::NEW_MAILS, $new_mails[MailService::NEW_MAILS]);
+
+		//TODO: put navigation handling here
+		return $mvc;		
+	}
 }
 
 ?>

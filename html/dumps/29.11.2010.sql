d0163ca51389113922e93dc3348da92d3f80a780
astabryn <astabryn@e5e4f178-09b3-11df-b317-0f3410dc57f6>
2010-11-29 17:01:18 +0000

    new dump

--- html/dumps/29.11.2010.sql
+++ html/dumps/29.11.2010.sql
@@ -43,8 +43,6 @@ CREATE TABLE `users` (
   `validation_id` varchar(32) NOT NULL,
   `role` varchar(2) NOT NULL,
   `avatar` varchar(255) default NULL,
-  `skype` varchar(255) default NULL,
-  `gender` varchar(6) default NULL,
   `company` varchar(255) default NULL,
   `vat` varchar(255) default NULL,
   `address` varchar(255) default NULL,
@@ -59,6 +57,8 @@ CREATE TABLE `users` (
   `bank` varchar(50) default NULL,
   `acoount_number` varchar(50) default NULL,
   `payment` tinyint(4) default NULL,
+  `salutation` tinyint(4) default NULL COMMENT '1- man; 2 -woman',
+  `company_year` int(11) default NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=45 ;
@@ -67,11 +67,10 @@ CREATE TABLE `users` (
 -- Дамп данных таблицы `users`
 -- 
 
-INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `username`, `password`, `crdate`, `modified`, `enabled`, `deleted`, `birthday`, `validation_id`, `role`, `avatar`, `skype`, `gender`, `company`, `vat`, `address`, `zip`, `location`, `region`, `country`, `phone`, `homepage`, `newsletter`, `send_job`, `bank`, `acoount_number`, `payment`) VALUES 
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `username`, `password`, `crdate`, `modified`, `enabled`, `deleted`, `birthday`, `validation_id`, `role`, `avatar`, `company`, `vat`, `address`, `zip`, `location`, `region`, `country`, `phone`, `homepage`, `newsletter`, `send_job`, `bank`, `acoount_number`, `payment`, `salutation`, `company_year`) VALUES 
 (4, 'Administrator', 'YouCademy', 'admin@youcademy.com', 'admin', '5f4dcc3b5aa765d61d8327deb882cf99', '2010-04-26 13:29:14', '2010-05-07 19:04:29', 1, 0, NULL, '840c3eda3ea42ecd90aeb3434f3510b7', 'AR', 'storage/uploads/users/admin/profile/avatar.jpg', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
 (37, 'bookkeeper', 'bookkeeper', 'bookkeeper@gmail.com', 'bookkeeper', '5f4dcc3b5aa765d61d8327deb882cf99', '2010-06-16 09:38:00', '2010-11-10 16:22:45', 1, 0, NULL, 'f69e505b08403ad2298b9f262659929a', 'BR', 'storage/uploads/users/bookkeeper/profile/avatar.jpg', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
 (41, 'John', 'Volt', 'john@gmail.com', 'promoter', '5f4dcc3b5aa765d61d8327deb882cf99', '2010-06-27 11:21:50', '2010-11-10 16:40:27', 1, 0, NULL, '4c8c76b39d294759a9000cbda3a6571a', 'PR', 'storage/uploads/users/user2/profile/avatar.jpg', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
 (40, 'Bill', 'Gates', 'bill@gmail.com', 'user1', '5f4dcc3b5aa765d61d8327deb882cf99', '2010-06-27 11:21:13', '2010-11-10 16:17:13', 1, 0, NULL, 'e0c7ccc47b2613c82d1073a4214deecc', 'UR', 'storage/uploads/users/user1/profile/avatar.jpg', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(42, 'Riky', 'Volly', 'rick@gmail.com', 'tradesman', '5f4dcc3b5aa765d61d8327deb882cf99', '2010-06-27 11:23:01', '2010-11-29 13:55:08', 1, 0, NULL, '309928d4b100a5d75adff48a9bfc1ddb', 'TR', 'storage/uploads/users/user4/profile/avatar.jpg', NULL, NULL, '', '', '', '', '', '', '', '', '', NULL, NULL, NULL, NULL, NULL),
-(43, 'Andrew', 'Stabryn', 'astabryn@gmail.com', 'astabryn', 'e10adc3949ba59abbe56e057f20f883e', '2010-10-07 13:42:52', '2010-11-10 16:17:09', 0, 0, '2000-09-07', '9a1de01f893e0d2551ecbb7ce4dc963e', 'UR', 'storage/uploads/users/user4/profile/avatar.jpg', '', 'Male', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(44, 'fdsfds', 'dfsfdsf', 'dasdas!@fdsf.das', 'dsaasd', '', '2010-11-10 16:08:45', '0000-00-00 00:00:00', 1, 0, NULL, '0f304eddb4ad6007a3093fd6d963a1d2', 'TR', 'storage/uploads/users/dsaasd/profile/avatar.jpg', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+(42, 'Riky', 'Volly', 'supprot@ito-global.com', 'tradesman', '5f4dcc3b5aa765d61d8327deb882cf99', '2010-06-27 11:23:01', '2010-11-29 18:51:07', 1, 0, NULL, '309928d4b100a5d75adff48a9bfc1ddb', 'TR', 'storage/uploads/users/tradesman/profile/avatar.jpg', 'ITO-Global', 'DH827265', 'st. Kokurudzy', '324342', 'lviv', 'lviv state', 'Ukraine', '0322493725', 'www.ito-global.com', NULL, NULL, NULL, NULL, NULL, 1, 1994),
+(43, 'Andrew', 'Stabryn', 'astabryn@gmail.com', 'astabryn', 'e10adc3949ba59abbe56e057f20f883e', '2010-10-07 13:42:52', '2010-11-10 16:17:09', 0, 0, '2000-09-07', '9a1de01f893e0d2551ecbb7ce4dc963e', 'UR', 'storage/uploads/users/user4/profile/avatar.jpg', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);

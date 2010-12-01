9b1b6803dab31fac627d6ea9c01957ef33284f0b
astabryn <astabryn@e5e4f178-09b3-11df-b317-0f3410dc57f6>
2010-12-01 12:38:45 +0000

    new dump

--- html/dumps/updates/30.11.2010.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE  `mails` ADD  `hash` VARCHAR( 16 ) NOT NULL ;
-ALTER TABLE  `mails` CHANGE  `status`  `status` TINYINT( 1 ) NOT NULL DEFAULT  '0' COMMENT  '1 - trash; 2 - drafts; 3 - inbox; 4 - outbox'

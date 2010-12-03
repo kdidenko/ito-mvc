8857dfbe274b8b5922fc2a69f29dee987881fa16
astabryn <astabryn@e5e4f178-09b3-11df-b317-0f3410dc57f6>
2010-12-03 23:27:19 +0000

    category and subcategory fuctionality

---
+++ branches/ito-b4u/html/com/itoglobal/eb4u/services/SubCategoryService.php
@@ -0,0 +1,78 @@
+<?php
+
+class SubCategoryService {
+	/**
+	 * @var string defining the mails table name
+	 */
+	const SUBCATEGORY = 'subcategory';
+	/**
+	 * @var  string defining the id field name
+	 */
+	const ID = 'id';
+	/**
+	 * @var  string defining the subject field name
+	 */
+	const CAT_NAME = 'subcategory_name';
+	/**
+	 * @var  string defining the subject field name
+	 */
+	const SUBCAT_ID = 'category_id';
+	
+	public static function getSubCategories ($where = NULL){
+		$fields = '*';
+		$from = self::SUBCATEGORY;
+		# executing the query
+		$result = DBClientHandler::getInstance ()->execSelect ( $fields, $from, $where, '' , '', '' );
+		$result = $result != null && isset($result) && count($result) > 0 ? $result : false;
+		return $result;
+	}
+	
+	public static function getSubcatByCat ($cat_id){
+		$fields = '*';
+		$from = self::SUBCATEGORY;
+		# executing the query
+		$where = self::SUBCAT_ID . '=' . $cat_id;
+		$result = DBClientHandler::getInstance ()->execSelect ( $fields, $from, $where, '' , '', '' );
+		$result = $result != null && isset($result) && count($result) > 0 ? $result : false;
+		return $result;
+	}
+
+	public static function updateSubCategory ($id, $fields, $vals){
+		$from = self::SUBCATEGORY;
+		$where = self::ID . " = '" . $id . "'";
+		# executing the query
+		DBClientHandler::getInstance ()->execUpdate ( $fields, $from, $vals, $where, '', '' );
+	}
+	
+	/**
+	 * Retrieves mail by specified mail id.
+	 * @param integer $hash the mail id
+	 * @return mail data
+	 */
+	public static function getSubCategory($id) {
+		$fields = '*';
+		$from = self::SUBCATEGORY;
+		$where = self::ID . '=' . $id;
+		# executing the query
+		$result = DBClientHandler::getInstance ()->execSelect ( $fields, $from, $where, '' , '', '' );
+		$result = $result != null && isset($result) && count($result) > 0 ? $result : false;
+		return $result;
+	}
+	
+	/**
+	 * delete mail from trash
+	 * @param integer $id the mail id
+	 */
+	public static function deleteCategory($id) {
+		# setting the query variables
+		$from = self::CATEGORY;
+		$where = self::ID . " = '" . $id . "'";
+		$orderBy = null;
+		$limit = null;
+		# executing the query
+		DBClientHandler::getInstance ()->execDelete($from, $where, $orderBy, $limit);
+	}	
+
+}
+
+?>

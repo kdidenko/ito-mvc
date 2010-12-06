f06c4a28c9daf02da7fef699b06c20298088c768
astabryn <astabryn@e5e4f178-09b3-11df-b317-0f3410dc57f6>
2010-12-06 13:38:44 +0000

    static block on my accounts new dump

--- branches/ito-b4u/html/dumps/updates/04.12.2010-2.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE  `users` ADD  `cat_id` INT NOT NULL ,
-ADD  `subcat_id` INT NOT NULL ;
--- branches/ito-b4u/html/dumps/updates/05.12.2010.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE  `static_block` (
- `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
- `block_title` VARCHAR( 255 ) NOT NULL ,
- `block_desc` TEXT NOT NULL
-) ENGINE = INNODB;

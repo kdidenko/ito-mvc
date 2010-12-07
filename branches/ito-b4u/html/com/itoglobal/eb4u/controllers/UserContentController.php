bae01b46eca334ed236fa5bf902e7b935c5c8bec
astabryn <astabryn@e5e4f178-09b3-11df-b317-0f3410dc57f6>
2010-12-07 22:05:59 +0000

    my profile for UR

--- branches/ito-b4u/html/com/itoglobal/eb4u/controllers/UserContentController.php
+++ branches/ito-b4u/html/com/itoglobal/eb4u/controllers/UserContentController.php
@@ -4,6 +4,79 @@ require_once 'com/itoglobal/eb4u/controllers/ContentController.php';
 
 class UserContentController extends ContentController {
 	
+	public function handleMyProfile($actionParams, $requestParams) {
+		$mvc = $this->handleActionRequest ( $actionParams, $requestParams );
+		$location = $this->onSuccess( $actionParams );
+		$id = SessionService::getAttribute ( SessionService::USERS_ID );
+		$error = array ();
+		if (isset ( $requestParams ['pswSbm'] )) {
+			$error [] .= isset($requestParams [UsersService::OLDPASSWORD]) ? UsersService::checkOldPassword ( $requestParams [UsersService::OLDPASSWORD] ) : false;
+			$error [] .= isset($requestParams [UsersService::PASSWORD]) ? UsersService::checkPassword ( $requestParams [UsersService::PASSWORD] ) : false;
+			$error [] .= isset($requestParams [UsersService::CONFIRM]) ? UsersService::checkConfirmPassword ( $requestParams [UsersService::PASSWORD], $requestParams [UsersService::CONFIRM] ) : false;
+			$error = array_filter ( $error );
+			
+			if (count ( $error ) == 0) {
+				if (isset($requestParams[UsersService::OLDPASSWORD]) && $requestParams[UsersService::OLDPASSWORD] != null){
+					$fields = UsersService::PASSWORD;
+					$from = UsersService::USERS;
+					$where = UsersService::ID . " = " . $requestParams[UsersService::ID];
+					# executing the query
+					$result = DBClientHandler::getInstance ()->execSelect ( $fields, $from, $where, '', '', '' );
+					if (md5($requestParams[UsersService::OLDPASSWORD]) == $result[0][UsersService::PASSWORD]){
+						$fields = array ();
+						$fields[] .= UsersService::PASSWORD;
+						$vals = array ();
+						$vals [] .= md5($requestParams [UsersService::PASSWORD]);
+						UsersService::updateFields ( $id, $fields, $vals );
+						$mvc->addObject ( self::STATUS, 'successful' );
+					} else {
+						$error [] .= '_i18n{You enter wrong old password. Please try again.}';
+						$mvc->addObject ( self::PSW_ERROR, $error );
+					}
+				}
+			}else{
+				$mvc->addObject ( self::PSW_ERROR, $error );
+			}
+		}
+		
+		if (isset ( $requestParams ['personalSbm'] )) {		
+			$fields = array (
+							'0'=>UsersService::FIRSTNAME, 
+							'1'=>UsersService::LASTNAME,
+							'2'=>UsersService::EMAIL,
+							'5'=>UsersService::ADDRESS,
+							'6'=>UsersService::ZIP,
+							'7'=>UsersService::LOCATION,
+							'8'=>UsersService::REGION,
+							'9'=>UsersService::COUNTRY,
+							'10'=>UsersService::PHONE,
+							'11'=>UsersService::SALUTATION
+							);
+			$vals = array (
+							'0'=>$requestParams [UsersService::FIRSTNAME], 
+							'1'=>$requestParams [UsersService::LASTNAME],
+							'2'=>$requestParams [UsersService::EMAIL],
+							'5'=>$requestParams [UsersService::ADDRESS],
+							'6'=>$requestParams [UsersService::ZIP],
+							'7'=>$requestParams [UsersService::LOCATION],
+							'8'=>$requestParams [UsersService::REGION],
+							'9'=>$requestParams [UsersService::COUNTRY],
+							'10'=>$requestParams [UsersService::PHONE],
+							'11'=>$requestParams [UsersService::SALUTATION]
+							);
+			
+			UsersService::updateFields ( $id, $fields, $vals );
+			$mvc->addObject ( self::STATUS, 'successful' );
+		}
+		
+		#get user info
+		$result = UsersService::getUser ( $id );
+		isset ( $result ) ? $mvc->addObject ( self::RESULT, $result ) : null;
+
+		return $mvc;
+	}
+	
+	
 	/*
 	private static function getUserRole(){
 		#prepeare value for sql query 

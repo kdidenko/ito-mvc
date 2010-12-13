0cc5cf5aea663f52a74f69440687fd33d5f7179a
astabryn <astabryn@e5e4f178-09b3-11df-b317-0f3410dc57f6>
2010-12-13 11:43:43 +0000

    last DB changes

--- branches/ito-b4u/html/dumps/updates/06.12.2010-3.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE  `users` ADD  `company_desc` TEXT NOT NULL AFTER  `company` ;
-UPDATE  `b4u`.`users` SET  `modified` = NOW( ) ,
-`company_desc` =  'we are good company' WHERE  `users`.`id` =42 LIMIT 1 ;

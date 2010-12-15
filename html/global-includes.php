cba075cceda7186d8cdffd0e59fabb826889339b
astabryn <astabryn@e5e4f178-09b3-11df-b317-0f3410dc57f6>
2010-12-15 20:02:30 +0000

    empty page for new order

--- html/global-includes.php
+++ html/global-includes.php
@@ -28,6 +28,7 @@
     require_once 'com/itoglobal/eb4u/services/PlanService.php';
     require_once 'com/itoglobal/eb4u/services/RemindService.php';
     require_once 'com/itoglobal/eb4u/services/RegionService.php';
+    require_once 'com/itoglobal/eb4u/services/CountryService.php';
     require_once 'com/itoglobal/eb4u/services/BargainsService.php';
     
     

60569f0be5f0699d4e11eb5401d27f883749149d
astabryn <astabryn@e5e4f178-09b3-11df-b317-0f3410dc57f6>
2011-07-03 19:19:01 +0000

    - Search form added - "Realestate All" functionality started

--- html/com/mls/content/RealestateController.php
+++ html/com/mls/content/RealestateController.php
@@ -3,6 +3,7 @@
 require_once ('com/itoglobal/mvc/defaults/BaseActionControllerImpl.php');
 
 class RealestateController extends BaseActionControllerImpl {
+	
 	/* (non-PHPdoc)
 	 * @see BaseActionControllerImpl::handleActionRequest()
 	 */
@@ -14,6 +15,32 @@ class RealestateController extends BaseActionControllerImpl {
 		$mvc->addObject ( 'regions', $regions );
 		return $mvc;
 	}
+	
+	/* (non-PHPdoc)
+	 * @see BaseActionControllerImpl::handleActionRequest()
+	 */
+	public function handleSearchRequest($actionParams, $requestParams) {
+		$mvc = $this->handleActionRequest ( $actionParams, $requestParams );
+		$country = $mvc->getProperty ( 'country' );
+		$state = $mvc->getProperty ( 'state' );
+		$regions = LocationService::citiesList ( $country, $state );
+		$mvc->addObject ( 'regions', $regions );
+		return $mvc;
+	}
+	
+	/* (non-PHPdoc)
+	 * @see BaseActionControllerImpl::handleActionRequest()
+	 */
+	public function handleListingsAll ($actionParams, $requestParams) {
+		$mvc = $this->handleActionRequest ( $actionParams, $requestParams );
+		$cnt = $mvc->getProperty('listingsCnt');
+		$listings = ListingService::getLatest($cnt);
+		$mvc->addObject('listings', $listings);
+		return $mvc;
+	}
+
+	
+	
 
 }
 
